7725e513da9fe4d32822f47df2ecf07b83141044
visheshtomarr <visheshtomar7800@gmail.com>
2025-02-23 01:34:35 +0530

    Added leaflet library to display map

--- script.js
+++ script.js
@@ -17,7 +17,17 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         const {latitude} = position.coords;
         const {longitude} = position.coords;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        const coords = [latitude, longitude];
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&key=YOUR_GOOGLE_API_KEY', {
+            attribution: '&copy; <a href="https://www.google.com/maps">Google Maps</a>',
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     }, function() {
         alert('Could not find your location!');
     })
